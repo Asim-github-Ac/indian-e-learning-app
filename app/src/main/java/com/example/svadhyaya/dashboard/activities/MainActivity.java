a241d30bda57ee4f41d3b48a69ea508522a178e4
Asim Mehar <asimmehar1931@gmail.com>
2021-10-15 03:48:53 -0700

    E Learing app with api

--- app/src/main/java/com/example/svadhyaya/dashboard/activities/MainActivity.java
+++ app/src/main/java/com/example/svadhyaya/dashboard/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.svadhyaya.dashboard.activities;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -13,11 +14,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.svadhyaya.R;
+import com.example.svadhyaya.Retrofit.APIClient;
+import com.example.svadhyaya.Retrofit.APIInterface;
+import com.example.svadhyaya.RetrofitModel.PackMoreDetails;
+import com.example.svadhyaya.RetrofitModel.PakagesDetails;
+import com.example.svadhyaya.RetrofitModel.UserLogin;
 import com.example.svadhyaya.SharedPrefrence.PrefManager;
 import com.example.svadhyaya.dashboard.fragments.HomeFragment;
 import com.example.svadhyaya.math.MathFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -27,27 +37,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private NavigationView mNavigationView;
     private BottomNavigationView mBottomNavigationView;
     PrefManager prefManager;
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initilization();
+        PackagesDetails();
         prefManager=new PrefManager(this);
+        apiInterface = APIClient.getClient().create(APIInterface.class);
         getFragment(new HomeFragment());
         mBottomNavigationView.setOnNavigationItemSelectedListener(bottomNav);
-        Toast.makeText(this, "User"+prefManager.getUser_name_info(), Toast.LENGTH_SHORT).show();
-        Log.d("tag", "onCreateView: em"+prefManager.getSave_Email_InFo());
-
-        System.out.println("g______________________"+prefManager.getUser_name_info());
     }
-
     @Override
     public void onClick(View view) {
 
+
+
+
     }
-
-
     public void initilization(){
         mDrawerLayout = findViewById(R.id.drawer);
         mNavigationView = findViewById(R.id.navigation_view);
@@ -88,5 +97,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mDrawerLayout.openDrawer(GravityCompat.START);
     }
 
+    public void PackagesDetails(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+        final PakagesDetails packdetails=new PakagesDetails("57316020e03f24759dce0fedeab4caa6");
+        Call<PakagesDetails> call= apiInterface.PackDetails(packdetails);
 
+        call.enqueue(new Callback<PakagesDetails>() {
+            @Override
+            public void onResponse(Call<PakagesDetails> call, Response<PakagesDetails> response) {
+                System.out.println("response"+response);
+                PakagesDetails details = response.body();
+                System.out.println(details.getMessage());
+                try {
+                    if (details != null && details.getStatus().equals("true")){
+
+
+                        System.out.println("yahooo ____________"+details.getUser().getPackMoreDetails().getEmail_id());
+
+
+                    }
+
+                }catch (Exception e){
+                    System.out.println("Expection __________"+e.getMessage());
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<PakagesDetails> call, Throwable t) {
+            progressDialog.dismiss();
+                System.out.println("errros fails______________"+t.getMessage());
+            }
+        });
+    }
 }
